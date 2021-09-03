1c1235fd598dfd8e24960813250615bcb50da8c9
KimWooSun <kipsong133@gmail.com>
2021-09-03 16:29:27 +0900

    폴더 구성 및 pod install

---
+++ Pods/Target Support Files/Pods-SeflMade_NewRx/Pods-SeflMade_NewRx-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_SeflMade_NewRxVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SeflMade_NewRxVersionString[];
+
---
+++ SeflMade_NewRx/README.md
@@ -0,0 +1,13 @@
+#  News API를 활용한 Rx_News 
+
+공개 API로 구성한 간단한 Rx 연습용 iOS 프로젝트입니다.
+
+## 구현 순서
+
+1. Coordinator 
+2. Model 
+3. API Service
+4. ViewModel
+5. UICollectionView 및 UICollectionViewCell
+
+
